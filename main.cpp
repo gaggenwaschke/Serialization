538de83c5a86efaefb6eec4c802263affeb0a7e7
Joshua <joshua@aconno.de>
2020-07-17 22:39:10 +0200

    Move Descriptor into MyClass

--- main.cpp
+++ main.cpp
@@ -19,31 +19,23 @@
 
 class MyClass
 {
-    friend class Descriptor;
-
 public:
     MyClass(int a, char b) : a(a), b(b)
     {
 
     }
 
-
-
 private:
     int a;
-    char b;    
-};
+    char b;
 
-class Descriptor
-{
 public:
     static Serialization::MemberDescriptor<MyClass, decltype(MyClass::a), &MyClass::a> descriptorA;
-    static Serialization::MemberDescriptor<MyClass, decltype(MyClass::b), &MyClass::b> descriptorB;
+    static Serialization::MemberDescriptor<MyClass, decltype(MyClass::b), &MyClass::b> descriptorB;    
 };
 
-Serialization::MemberDescriptor<MyClass, decltype(MyClass::a), &MyClass::a> Descriptor::descriptorA{"a"};
-Serialization::MemberDescriptor<MyClass, decltype(MyClass::b), &MyClass::b> Descriptor::descriptorB{"b"};
-
+Serialization::MemberDescriptor<MyClass, decltype(MyClass::a), &MyClass::a> MyClass::descriptorA{"a"};
+Serialization::MemberDescriptor<MyClass, decltype(MyClass::b), &MyClass::b> MyClass::descriptorB{"b"};
 
 //-------------------------------- CONSTANTS ----------------------------------
 
@@ -56,11 +48,11 @@ int main(int argc, char* argv[], char* env[])
     MyClass mc1{1,2};
     MyClass mc2{3,4};
 
-    std::cout << Descriptor::descriptorA.getMemberValue(mc1) << std::endl;
-    std::cout << Descriptor::descriptorB.getMemberValue(mc1) << std::endl;
+    std::cout << MyClass::descriptorA.getMemberValue(mc1) << std::endl;
+    std::cout << MyClass::descriptorB.getMemberValue(mc1) << std::endl;
 
-    std::cout << Descriptor::descriptorA.getMemberValue(mc2) << std::endl;
-    std::cout << Descriptor::descriptorB.getMemberValue(mc2) << std::endl;
+    std::cout << MyClass::descriptorA.getMemberValue(mc2) << std::endl;
+    std::cout << MyClass::descriptorB.getMemberValue(mc2) << std::endl;
     return 0;
 }
 
